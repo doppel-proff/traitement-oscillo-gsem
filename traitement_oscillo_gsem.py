6693bf35d4a5912e36ce24f851680172232994bc
MATTHIAS URSET <matthias.urset@skf.com>
2025-12-08 17:29:03 +0100

    clean up

--- traitement_oscillo_gsem.py
+++ traitement_oscillo_gsem.py
@@ -10,13 +10,14 @@ import files_utils as fu
 import list_utils as lu
 import tekfiles_utils as tu
 import graph_utils as gu
+import math_utils as mu
 import matplotlib.pyplot as plt
 
 Path=os.getcwd()
 
 #ouverture des fichiers
 Ubs_o = fu.open_csv("mesures\ess 10 bear volt.csv")
-Ibi_o = fu.open_csv("mesures\ess 10 bear cur out.csv")
+Ibi_o = fu.open_csv("mesures\ess 10 bear cur in.csv")
 Ibo_o = fu.open_csv("mesures\ess 10 bear cur out.csv")
 
 #Matrice [[temps],[Courant entrée]]
@@ -32,23 +33,23 @@ Ubs = lu.enlever_n_premiers_points(Ubs_o,13)
 XIbi = tu.absi(Ibi)
 XIbi = lu.scale(XIbi,1e6)
 
-#vecteur temps des courants en entrée
+#vecteur des temps courants en sortie 
 XIbo = tu.absi(Ibo)
 XIbo = lu.scale(XIbo,1e6)
 
-#vecteur temps des courants en entrée
+#vecteur temps des tensions 
 XUbs = tu.absi(Ubs)
 XUbs = lu.scale(XUbs,1e6)
 
-#vecteur temps des courants en entrée
+#vecteur des courants en entrée
 YIbi = tu.ordi(Ibi)
 YIbi = lu.scale(YIbi, 1e3)
 
-#vecteur temps des courants en entrée
+#vecteur des courants en sortie 
 YIbo = tu.ordi(Ibo)
 YIbo = lu.scale(YIbo, 1e3)
 
-#vecteur temps des courants en entrée
+#vecteur des tensions
 YUbs = tu.ordi(Ubs)
 
 M=[[XIbi,YIbi],[XIbo,YIbo],[XUbs,YUbs]]
@@ -66,12 +67,13 @@ Y_Axes = ["Courant en entrée des roulements en mA","Courant en sortie des roule
 #Titre axe des abscises 
 X_Axes = ["Temps en us","Temps en us","Temps en us"]
 
-#gu.graphsup(M,Path,Repo,Fig_name,Y_Axes,X_Axe)
 gu.multigraph(M,Path,Repo_ind,Fig_name,Y_Axes,X_Axes)
 
 M2=[[XIbi,YIbi],[XIbo,YIbo]]
-gu.graphsup(M2,Path,Repo_sup,"Comparaison des courants en entré et sortie",["Test #10_Io mA","Test #10_Is mA"],"Temps en us")
+gu.graphsup(M2,Path,Repo_sup,"Comparaison des courants en entrée et sortie",["Test #10_Io mA","Test #10_Is mA"],"Temps en us")
 
+"""
 print(YIbi[0])
 print(YIbo[0])
 print(YUbs[0])
+"""
